6e086cdb1f26800f9145fe69af47ae4d6a0a56c8
mjthurber <mjthurber@gmail.com>
2024-03-07 11:40:47 -0700

    added banner

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,13 +1,7 @@
-// import React from "react";
-// Bring in accompanying css file
 import "./Navbar.css";
 import {useLocation} from 'react-router-dom';
-
-// Import logo and cart icon from assets folder
 import logo from "../../assets/logo.png";
 import Cart from '../Cart';
-
-// import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -18,40 +12,45 @@ function Navigation() {
   const currentPage = useLocation().pathname;
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            src={logo}
-            alt="Logo"
-            width="200"
-            height="100"
-            className="d-inline-block align-text-top"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle className="navbar-light bg-light" aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/" className={currentPage === '/' ? 'nav-link-active' : 'nav-link'}>Home</Nav.Link>
-            <Nav.Link href="/about" className={currentPage === '/about' ? 'nav-link-active' : 'nav-link'}>About Us</Nav.Link>
-            <Nav.Link href="/candles" className={currentPage === '/candles' ? 'nav-link-active' : 'nav-link'}>Shop Candles</Nav.Link>
-            <Nav.Link href="/bundles" className={currentPage === '/bundles' ? 'nav-link-active' : 'nav-link'}>Shop Bundles</Nav.Link>
-            <Nav.Link href="/contact" className={currentPage === '/contact' ? 'nav-link-active' : 'nav-link'}>Contact</Nav.Link>
-          </Nav>
-          <Nav className='r-nav'>
-            {Auth.loggedIn() ? ( 
-              <Nav>
-                <Nav.Link href="/orderHistory" className={currentPage === '/orderHistory' ? 'nav-link-active' : 'nav-link'}>Order History</Nav.Link>
-                <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
-              </Nav>
-            ) : (
-            <Nav.Link href="/login" className={currentPage === '/login' ? 'nav-link-active' : 'nav-link'}>Login</Nav.Link>
-            )}
-            <Cart />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <div className="banner">
+        Free Shipping on Orders $50+
+      </div>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              alt="Logo"
+              width="200"
+              height="100"
+              className="d-inline-block align-text-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle className="navbar-light bg-light" aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className={currentPage === '/' ? 'nav-link-active' : 'nav-link'}>Home</Nav.Link>
+              <Nav.Link href="/about" className={currentPage === '/about' ? 'nav-link-active' : 'nav-link'}>About Us</Nav.Link>
+              <Nav.Link href="/candles" className={currentPage === '/candles' ? 'nav-link-active' : 'nav-link'}>Shop Candles</Nav.Link>
+              <Nav.Link href="/bundles" className={currentPage === '/bundles' ? 'nav-link-active' : 'nav-link'}>Shop Bundles</Nav.Link>
+              <Nav.Link href="/contact" className={currentPage === '/contact' ? 'nav-link-active' : 'nav-link'}>Contact</Nav.Link>
+            </Nav>
+            <Nav className='r-nav'>
+              {Auth.loggedIn() ? (
+                <Nav>
+                  <Nav.Link href="/orderHistory" className={currentPage === '/orderHistory' ? 'nav-link-active' : 'nav-link'}>Order History</Nav.Link>
+                  <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
+                </Nav>
+              ) : (
+                <Nav.Link href="/login" className={currentPage === '/login' ? 'nav-link-active' : 'nav-link'}>Login</Nav.Link>
+              )}
+              <Cart />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
